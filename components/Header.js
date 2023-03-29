d41c084106700bbdc35901bb449abc4dbf9ed399
EasyPeasyGH <nomo84@gmx.de>
2023-03-29 09:56:36 +0200

    bug fix search

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ export default function Header({ search, toggleSearch }) {
   const router = useRouter();
   const { push, pathname } = router;
 
+  console.log("push pathname", typeof pathname);
   return (
     <header>
       <div className="headerBar__box">
@@ -27,24 +28,24 @@ export default function Header({ search, toggleSearch }) {
         <Link href="/">Capstone</Link>
       </h1>
       <div className="headerBar__box">
-        {pathname === "/" && (
-          <div className="icon unavailable">
-            <Link
-              href="/"
-              target="_self"
-              alt="Toggle search bar"
-              onClick={(event) => {
-                event.preventDefault();
-                search = !search;
-                toggleSearch(search);
-              }}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-              </svg>
-            </Link>
-          </div>
-        )}
+        <div className="icon">
+          {/* <div {pathname === "/" ? {className="icon"} : {className="icon unavailable"}}> */}
+          <Link
+            className={pathname !== "/" && "unavailable"}
+            href="/"
+            target="_self"
+            alt="Toggle search bar"
+            onClick={(event) => {
+              event.preventDefault();
+              search = !search;
+              toggleSearch(search);
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+            </svg>
+          </Link>
+        </div>
         <div className="icon unavailable">
           <Link href="/shoppingcard" target="_self" alt="Link to shopping card">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
